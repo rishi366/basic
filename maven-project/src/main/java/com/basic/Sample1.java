b229d1ecea9c2e047ff960829d39e2dd3bd8ad5c
rishi366 <109204454+rishi366@users.noreply.github.com>
2022-07-13 12:44:21 +0530

    Add files via upload

---
+++ maven-project/src/main/java/com/basic/Sample1.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class Sample1 {
+	public String Sample() {
+		return "sample";
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Hello Java");
+	}
+
+}
---
+++ maven-project/src/test/java/com/basic/Sample1Test.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Sample1Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
